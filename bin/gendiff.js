9294c27acde4db90e1f48617b681972d691b6b70
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-08-21 20:06:01 +0300

    Step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version','output version information')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse();
