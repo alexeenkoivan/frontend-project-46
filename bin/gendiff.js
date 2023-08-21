28fb07f1ec82d1ebd5daacf2cb7c9a9a2d899a32
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-08-21 19:50:28 +0300

    Step1(fix)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,6 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-h, --help', 'output usage information');
-
-program.parse();
+  .version('1.0.0', '-V, --version','output version information')
+  .helpOption('-h, --help', 'output usage information')
+  .parse();
