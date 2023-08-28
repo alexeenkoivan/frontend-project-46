71ac3c5cc39e8b5f99a62b49c26c3a08a7b5ea50
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-08-28 22:23:32 +0300

    Step4.1

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  setupFiles: ['dotenv/config'], // Если вы используете dotenv
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.mjs$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'mjs', 'jsx', 'json', 'node'],
-};
