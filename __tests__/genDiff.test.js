ba82880168f4283ced85925d5618e42d219d80a1
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-09-03 17:26:38 +0300

    Step4 fin

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,17 +1,17 @@
 import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const filePath1 = path.resolve(__dirname, '__fixtures__/file1.json');
 const filePath2 = path.resolve(__dirname, '__fixtures__/file2.json');
 
 test('genDiff() compares two JSON files and produces the expected result', () => {
-  const expectedDiff = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true 
-  }`;
+  const expectedDiff = fs.readFileSync(
+    path.resolve(__dirname, '__fixtures__/expectedJsonFormat'), 'utf-8'
+  );
   expect(genDiff(filePath1, filePath2)).toEqual(expectedDiff);
 });
